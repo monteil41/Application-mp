2d6d5f9310357d88dc19e323b2e51b31ce21f7c6
monteil41 <pierre-hugo.monteil@etu.unilim.fr>
2018-05-18 12:21:30 +0200

    pop up + localisation porcelaine

--- src/providers/js-maps/js-maps.ts
+++ src/providers/js-maps/js-maps.ts
@@ -26,28 +26,41 @@ export class JsMapsProvider {
     //------------------------ Ici les marqueurs-------------------------//
 
     //--- declarations des marqueurs ---//
+
+    // utilisateur
     this.addMarker_User(this.map);
-    this.addMarker_Test(this.map);
-    this.addMarker_Test2(this.map);
+    // madeleine
+
+
+    // porcelaine
+    this.addMarker_caffy(this.map);
+    this.addMarker_brindherbe(this.map);
+    this.addMarker_marcognac(this.map);
+    this.addMarker_laseynie(this.map);
+    this.addMarker_salomon(this.map);
+    this.addMarker_couchou(this.map);
+    this.addMarker_patrimoine(this.map);
+    this.addMarker_samie(this.map);
+    this.addMarker_samie2(this.map);
+    this.addMarker_palloux(this.map);
   }
 
 
     //--- Ajout des marqueurs ---//
 
+//-----------------------UTILISATEUR--------------------------------//
+addMarker_User(map:any){
 
-
-  addMarker_User(map:any){
-
-    let marker = new google.maps.Marker({
-    map: map,
-    animation: google.maps.Animation.DROP,
-    position: map.getCenter(),
-    icon: "assets/imgs/officetourisme2.png"
-    });
-  }
-
-  addMarker_Test(map:any){
-    var myLatLng = {lat: 45.512, lng: 1.203};
+  let marker = new google.maps.Marker({
+  map: map,
+  animation: google.maps.Animation.DROP,
+  position: map.getCenter(),
+  icon: "assets/imgs/you.png"
+  });
+}
+//-----------------------PORCELAINE---------------------------------//
+  addMarker_brindherbe(map:any){
+    var myLatLng = {lat: 45.519633, lng: 1.203393};
     let marker = new google.maps.Marker({
     map: map,
     animation: google.maps.Animation.DROP,
@@ -55,14 +68,100 @@ export class JsMapsProvider {
     });
   }
 
-  addMarker_Test2(map:any){
+  addMarker_caffy(map:any){
 
-    var myLatLng = {lat: 45.512, lng: 1.202};
+    var myLatLng = {lat: 45.429, lng: 1.302};
     let marker = new google.maps.Marker({
     map: map,
     animation: google.maps.Animation.DROP,
     position: myLatLng,
-    icon: "assets/imgs/test2.png"
     });
   }
+
+  addMarker_marcognac(map:any){
+
+    var myLatLng = {lat: 45.519261, lng: 1.251772};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_laseynie(map:any){
+
+    var myLatLng = {lat: 45.508028, lng: 1.217566};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_salomon(map:any){
+
+    var myLatLng = {lat: 45.502287, lng: 1.206459};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_couchou(map:any){
+    var contentString = "coucou"
+    var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+    var myLatLng = {lat: 45.514181, lng: 1.201096};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+    marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  }
+
+  addMarker_patrimoine(map:any){
+
+    var myLatLng = {lat: 45.515729, lng: 1.202694};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_samie(map:any){
+
+    var myLatLng = {lat: 45.514381, lng: 1.202118};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_samie2(map:any){
+
+    var myLatLng = {lat: 45.559212, lng: 1.209384};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+
+  addMarker_palloux(map:any){
+
+    var myLatLng = {lat: 45.531414, lng: 1.204521};
+    let marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: myLatLng,
+    });
+  }
+  //-----------------------MADELEINE---------------------------------//
 }
